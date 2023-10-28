8b3ce3a74e63ddcb074cd3de4dc0a54cdf19d3fd
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2023-10-28 21:14:47 +0100

    storybook installed

---
+++ frontend/listit-lol/.storybook/main.ts
@@ -0,0 +1,37 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const tsconfigPaths = require('vite-tsconfig-paths');
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  /**
+   * A option exposed by storybook-builder-vite for customising the Vite config.
+   * @see https://github.com/eirslett/storybook-builder-vite#customize-vite-config
+   * @param {import("vite").UserConfig} config
+   * @see https://vitejs.dev/config/
+   */
+  viteFinal: (config) => {
+    return {
+      ...config,
+      plugins: [...(config.plugins || []), tsconfigPaths.default()],
+    };
+  },
+};
+export default config;

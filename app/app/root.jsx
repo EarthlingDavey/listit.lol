4486f49d0e802c4014e04a9c1dc1032e2a5e8d8e
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2023-10-28 20:53:09 +0100

    frontend listit-lol working with remix tailwind and shadcn

---
+++ app/app/root.jsx
@@ -0,0 +1,21 @@
+import { Links, LiveReload, Meta, Outlet, Scripts } from '@remix-run/react';
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Hello world!</h1>
+        <p>Words 3 </p>
+        <Outlet />
+
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}

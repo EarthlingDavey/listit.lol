c9065c8cf9030798d998cb908483ead15206b4c1
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2023-10-31 13:00:57 +0000

    📕Storybook: Chromatic: Remove logs

--- frontend/app/components/MySideBar.tsx
+++ frontend/app/components/MySideBar.tsx
@@ -5,8 +5,6 @@ export function SideBar() {
     <div className="fixed left-0 top-0 flex h-screen w-16 flex-col bg-white text-white shadow-lg dark:bg-gray-700">
       <SideBarIcon icon={<FaFire size="28" />} />
       <SideBarIcon icon={<FaFire size="28" />} />
-      <SideBarIcon icon={<FaFire size="28" />} />
-      <SideBarIcon icon={<FaFire size="28" />} />
     </div>
   );
 }
